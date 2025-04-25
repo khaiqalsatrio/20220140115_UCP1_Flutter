265fce9efc18fedba28e996b553d0d748a71d6e3
Khaiqol <Khaiqol@gmail.com>
2025-04-25 13:50:22 +0700

    memebuat class login page dan state

---
+++ lib/presentation/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // Controller untuk menangani input dari TextField email dan password
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+}
