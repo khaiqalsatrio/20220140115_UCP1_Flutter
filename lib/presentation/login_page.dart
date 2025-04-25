9e25508be393ac61199eb3e04e4f497eed73a76f
Khaiqol <Khaiqol@gmail.com>
2025-04-25 14:35:18 +0700

    meperbaiki login page

--- lib/presentation/login_page.dart
+++ lib/presentation/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ucp1_115/presentation/home_page.dart';
 import 'package:flutter_ucp1_115/presentation/register_page.dart';
 
 class LoginPage extends StatefulWidget {
