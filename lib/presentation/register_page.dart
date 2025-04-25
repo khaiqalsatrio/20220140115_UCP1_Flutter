a8ccc966c9f33445da556fbc7f124710eff51bb1
Khaiqol <Khaiqol@gmail.com>
2025-04-25 13:59:01 +0700

    memebuat class register page

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -6,3 +6,12 @@ class RegisterPage extends StatefulWidget {
   @override
   State<RegisterPage> createState() => _RegisterPageState();
 }
+
+class _RegisterPageState extends State<RegisterPage> {
+  // Controller untuk mengambil input dari TextField email dan password
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // GlobalKey digunakan untuk mengakses dan memvalidasi Form
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+}
