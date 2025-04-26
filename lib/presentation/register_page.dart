72d5291569b4a57d053ab5285ec78d5981514b88
Khaiqol <Khaiqol@gmail.com>
2025-04-26 12:08:14 +0700

    perbaikan register page

--- lib/presentation/register_page.dart
+++ lib/presentation/register_page.dart
@@ -8,78 +8,112 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController namalengkapController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
+  final TextEditingController nohpController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  final TextEditingController phoneController =
-      TextEditingController(); // Controller untuk nomor HP
+  final TextEditingController confirmpasswordController =
+      TextEditingController();
 
   @override
   void initState() {
-    super.initState(); // Perbaikan typo dari 'iniState' ke 'initState'
+    super.initState();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      // Jika valid, proses registrasi bisa dilakukan di sini
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('Proses Registrasi...')));
+
+      // masuk ke halaman login setelah register
+      Navigator.pushNamed(context, '/login');
+    } else {
+      // Validasi
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Harap lengkapi data dengan benar')),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Form(
+        key: _formKey,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Register'),
+              const Text('Register', style: TextStyle(fontSize: 24)),
+              TextFormField(
+                controller: namalengkapController,
+                decoration: const InputDecoration(labelText: 'Nama lengkap'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Nama lengkap tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
               TextFormField(
                 controller: emailController,
                 decoration: const InputDecoration(labelText: 'Email'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
+                    return 'Email tidak boleh kosong';
                   }
                   return null;
                 },
               ),
               TextFormField(
-                controller: phoneController, // Field untuk nomor HP
-                decoration: const InputDecoration(labelText: 'No. HP'),
-                keyboardType:
-                    TextInputType.phone, // Untuk mempermudah input nomor
+                controller: nohpController,
+                decoration: const InputDecoration(labelText: 'Nomor Hp'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter your phone number';
+                    return 'Nomor Hp tidak boleh kosong';
                   }
                   return null;
                 },
               ),
               TextFormField(
                 controller: passwordController,
-                decoration: const InputDecoration(labelText: 'Password'),
+                decoration: const InputDecoration(labelText: 'password'),
                 obscureText: true,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter your password';
+                    return 'Password tidak boleh kosong';
                   }
                   return null;
                 },
               ),
               TextFormField(
-                controller: passwordController,
+                controller: confirmpasswordController,
                 decoration: const InputDecoration(
-                  labelText: 'Confirm Password',
+                  labelText: 'Confirm password',
                 ),
                 obscureText: true,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
-                    return 'Please enter your Confirm password';
+                    return 'Confirm password tidak boleh kosong';
                   }
                   return null;
                 },
               ),
-              ElevatedButton(onPressed: () {}, child: Text('Register')),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: _submitForm, // Panggil _submitForm di sini
+                child: const Text('Register'),
+              ),
               TextButton(
                 onPressed: () {
                   Navigator.pushNamed(context, '/login');
                 },
-                child: Text('Kamu Sudah punya akun? Login sekarang'),
+                child: const Text('Kamu sudah punya akun? Login sekarang'),
               ),
             ],
           ),
